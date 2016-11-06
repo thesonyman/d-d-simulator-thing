83962fb972e9dfa4e3d6b5b54c2c7d0b24db9868
Zach <thesonyman101@gmail.com>
2016-11-05 20:35:48 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+import os, random, time
+
+hit = 0
+
+humans = 10
+hum_health = 15
+
+en_Guards = 5
+en_health = 15
+x = 1000
+enwin = 0
+gwin = 0
+while x > 0:
+    z = False
+    print ("repeat")
+    while en_Guards and humans > 0:
+        print ("start")
+        time.sleep(1)
+        while z == False:
+            print ("1")
+
+            hit = random.randint(1, 20)
+            if hit >= 10:
+                en_health = en_health - random.randint(1, 4)
+                if en_health >= 0:
+                    en_Guards = en_Guards - 1
+                    #print("dead guard")
+                    en_health = 15
+            hit = random.randint(1, 20)
+            if hit >= 10:
+                hum_health = hum_health - random.randint(1, 8)
+                if hum_health >= 0:
+                    humans = humans - 1
+                    #print("dead human")
+                    hum_health = 15
+            if humans <= 0:
+                #print ("Guards win")
+                enwin = enwin + 1
+                human = 10
+                z = True
+
+
+            if en_Guards <= 0:
+                #print ("Humans win")
+                gwin = gwin + 1
+                en_Guards = 5
+                z = True
+
+
+    x = x - 1
+    print x
+    if x == 0:
+        print gwin / 1000
+        print enwin / 1000
