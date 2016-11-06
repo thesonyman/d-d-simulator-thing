84641f37155edc0e184ce6cd3f0d121ac2cdd792
Zach <thesonyman101@gmail.com>
2016-11-05 21:27:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,47 +8,56 @@ hum_health = 15
 en_Guards = 5
 en_health = 15
 x = 1000
-enwin = 0
-gwin = 0
-while x > 0:
+gwin = 0.0
+enwin = 0.0
+for i in range(0, 1000):
     z = False
-    print ("repeat")
-    while en_Guards and humans > 0:
-        print ("start")
-        time.sleep(1)
+    b = True
+    #print ("repeat")
+    while b == True:
+        #print ("start")
+        #time.sleep(1)
+        z = False
         while z == False:
-            print ("1")
+            #print ("1")
 
             hit = random.randint(1, 20)
             if hit >= 10:
                 en_health = en_health - random.randint(1, 4)
-                if en_health >= 0:
-                    en_Guards = en_Guards - 1
+            if en_health >= 0:
+                en_Guards = en_Guards - 1
                     #print("dead guard")
-                    en_health = 15
+                en_health = 15
             hit = random.randint(1, 20)
             if hit >= 10:
                 hum_health = hum_health - random.randint(1, 8)
-                if hum_health >= 0:
-                    humans = humans - 1
-                    #print("dead human")
-                    hum_health = 15
+            if hum_health >= 0:
+                humans = humans - 1
+                #print("dead human")
+                hum_health = 15
+
             if humans <= 0:
                 #print ("Guards win")
                 enwin = enwin + 1
                 human = 10
                 z = True
-
+                b = False
 
             if en_Guards <= 0:
                 #print ("Humans win")
                 gwin = gwin + 1
                 en_Guards = 5
                 z = True
+                b = False
+
 
 
     x = x - 1
-    print x
-    if x == 0:
-        print gwin / 1000
-        print enwin / 1000
+    #print x
+
+    if x <= 0:
+        print ("Done")
+        cats = gwin
+        dogs = enwin
+        print cats
+        print dogs
